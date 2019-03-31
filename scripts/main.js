8904c14f37f79c364520d207d88ad292fe3ae7f7
wenyivet <wenyivetw@gmail.com>
2019-03-31 22:43:43 +0200

    Update API and clean code

--- scripts/main.js
+++ scripts/main.js
@@ -28,12 +28,6 @@ function getAPIdata() {
 		var image = 'https://epic.gsfc.nasa.gov/archive/natural/'+ year +'/'+month+'/'+day+'/png/'+ response[0].image +'.png';
 
 		document.body.style.backgroundImage = "url('"+ image +"')";
-
-		//console.log('https://epic.gsfc.nasa.gov/'+ response[0].image +'.jpg');
-
-
-		// render weatherCondition
-		//onAPISucces(response);
 	})
 	
 	// catch error
@@ -42,36 +36,33 @@ function getAPIdata() {
 		console.error('Request failed', error);
 	});
 }
-//http://api.openweathermap.org/data/2.5/weather?q=The%20Hague,nl&appid=0a51065693a4d2c800cd740ee0467c1e&units=metric
-//http://api.openweathermap.org/data/2.5/weather?q=Oranjestad,aw&appid=0a51065693a4d2c800cd740ee0467c1e&units=metric
-//http://api.openweathermap.org/data/2.5/weather?q=Washington,us&appid=0a51065693a4d2c800cd740ee0467c1e&units=metric
 function getWeatherAPIdata(id) {
 	var land="";
 	var stad="";
 	var request="";
 	switch(id){
 		case 1:
-			request= 'http://api.openweathermap.org/data/2.5/weather?q=London,gb&appid=0a51065693a4d2c800cd740ee0467c1e&units=metric';
+			request= 'https://api.openweathermap.org/data/2.5/weather?q=London,gb&appid=0a51065693a4d2c800cd740ee0467c1e&units=metric';
 			land= "Verenigde koninkrijk";
 			stad= "Londen"; 
 			break;
 		case 2:
-			request= 'http://api.openweathermap.org/data/2.5/weather?q=The%20Hague,nl&appid=0a51065693a4d2c800cd740ee0467c1e&units=metric';
+			request= 'https://api.openweathermap.org/data/2.5/weather?q=The%20Hague,nl&appid=0a51065693a4d2c800cd740ee0467c1e&units=metric';
 			land= "Nederland";
 			stad= "Den Haag"; 
 			break;
 		case 3:
-			request= 'http://api.openweathermap.org/data/2.5/weather?q=Oranjestad,aw&appid=0a51065693a4d2c800cd740ee0467c1e&units=metric';
+			request= 'https://api.openweathermap.org/data/2.5/weather?q=Oranjestad,aw&appid=0a51065693a4d2c800cd740ee0467c1e&units=metric';
 			land= "Aruba";
 			stad= "Oranjestad"; 
 			break;
 		case 4:
-			request= 'http://api.openweathermap.org/data/2.5/weather?q=Washington,us&appid=0a51065693a4d2c800cd740ee0467c1e&units=metric';
+			request= 'https://api.openweathermap.org/data/2.5/weather?q=Washington,us&appid=0a51065693a4d2c800cd740ee0467c1e&units=metric';
 			land= "Verenigde staten";
 			stad= "Washington DC"; 
 			break;
 		default:
-			request= 'http://api.openweathermap.org/data/2.5/weather?q=The%20Hague,nl&appid=0a51065693a4d2c800cd740ee0467c1e&units=metric';
+			request= 'https://api.openweathermap.org/data/2.5/weather?q=The%20Hague,nl&appid=0a51065693a4d2c800cd740ee0467c1e&units=metric';
 			land= "Nederland";
 			stad= "Den Haag"; 
 			break;
@@ -111,12 +102,6 @@ function getWeatherAPIdata(id) {
 			document.getElementById('advice').innerHTML= 'Niet veilig om te landen!';
 			document.getElementById('alertImg').src = "./img/warning.png";
 		}
-		// set main(vb Cloud, Sun...) into html
-		// set temp
-		// set visibility
-		// set wind speed
-		// render weatherCondition
-		//onAPISucces(response);
 	})
 	
 	// catch error
@@ -127,5 +112,4 @@ function getWeatherAPIdata(id) {
 }
 
 getAPIdata();
-/*getWeatherLondenAPIdata();*/
 
